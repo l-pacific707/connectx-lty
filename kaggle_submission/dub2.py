9dadad05170e3e2bb418613829b7d293d48dabf8
l-pacific707 <sun060104@gmail.com>
2025-05-12 02:41:42 +0900

    remove redundants

--- kaggle_submission/dub2.py
+++
@@ -1,5 +0,0 @@
-import zipfile
-
-with zipfile.ZipFile("submission.zip", "r") as z:
-    print("📦 Zip 파일 안의 내용:")
-    z.printdir()
--- kaggle_submission/dummy.py
+++
@@ -1,8 +0,0 @@
-import zipfile
-
-with zipfile.ZipFile("submission.zip", "w", zipfile.ZIP_DEFLATED) as zipf:
-    zipf.write("main.py")
-    zipf.write("mydata.pkl.xz")
-    zipf.write("MCTS_sumbit.py")
-
-print("submission.zip created successfully.")
